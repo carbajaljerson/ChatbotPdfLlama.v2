1e5a2b94f74b948aa63b8f2ec8b8e1a83e348696
Jerson Carbajal <jerson182@hotmail.com>
2023-10-09 05:20:23 -0500

    fixing app.py

--- app.py
+++ app.py
@@ -1,69 +1,49 @@
-import streamlit as st 
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM 
-from transformers import pipeline
-import torch
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader, PDFMinerLoader 
-from langchain.text_splitter import RecursiveCharacterTextSplitter 
+import streamlit as st
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import Docx2txtLoader
 from langchain.embeddings import SentenceTransformerEmbeddings 
 from langchain.vectorstores import Chroma 
-from langchain.llms import HuggingFacePipeline
-from langchain.chains import RetrievalQA
-from langchain.llms import CTransformers
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-#from streamlit_chat import message
-import os 
+import os
+#import tempfile
+import utils as utils
+import importlib
+importlib.reload(utils)
 
 ABS_PATH: str = os.path.dirname(os.path.abspath(__file__))
 DB_DIR: str = os.path.join(ABS_PATH, "db")
 st.set_page_config(layout="wide")
 
 
-
-@st.cache_resource
-def qa_llm():
-   
-    llm = CTransformers(model="./model/llama-2-7b-chat.ggmlv3.q4_0.bin",
-                        streaming=True, 
-                        callbacks=[StreamingStdOutCallbackHandler()],
-                        model_type="llama", config={'max_new_tokens': 512, 'temperature': 0.001})
-    
-    
-    embeddings = SentenceTransformerEmbeddings(model_name="./model/all-MiniLM-L6-v2",
-                                               model_kwargs={'device': 'cpu'})
-    db = Chroma(persist_directory=DB_DIR, embedding_function = embeddings)
-    retriever = db.as_retriever()
-    qa = RetrievalQA.from_chain_type(
-        llm = llm,
-        chain_type = "stuff",
-        retriever = retriever,
-        return_source_documents=True
-    )
-    return qa
-
-def process_answer(instruction):
-    answer = None
-    instruction = instruction
-    qa = qa_llm()
-    generated_text = qa(instruction)
-    answer = generated_text['result']
-    return answer
-
-
 def main():
-    st.markdown("<h1 style='text-align: center; color: blue;'>Chat with your PDF 🦜📄 </h1>", unsafe_allow_html=True)
+    '''
+    Main function for the Streamlit-based ChatBot application.
 
-    with st.expander("About the App"):
-        st.markdown("""
-                    This is a Generative AI powered Question and Anwering app that responds question 
-                    about your PDF file
-                    """
-        )
-    question = st.text_area("Enter YourQuestion")
-    if st.button("Search"):
-        st.info("Your question: "+question )
-        st.info("Your Answer")
-        answer = process_answer(question)
-        st.write(answer)
+    This function sets up and runs the ChatBot application, which allows users to upload and process
+    multiple documents, create embeddings, and interact with a conversational ChatBot.
+
+    Returns:
+        None
+    '''
+
+    # Initialize session state
+    utils.initializeSessionState()
+    st.title("ChatBot de documentos usando llama2 :books:")   
+   
+    # Create embeddings  
+    embeddings = SentenceTransformerEmbeddings(model_name="./model/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+
+    # Create vector store
+    vectorStore =  Chroma(persist_directory=DB_DIR, embedding_function = embeddings)
+    
+    # Create the chain object
+    chain = utils.createConversationalChain(vectorStore)
+
+    utils.displayChatHistory(chain)
+    
         
 if __name__ == "__main__":
     main()
